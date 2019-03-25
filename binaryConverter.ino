9693e2dab21f774fdb36382b80c9db709a99454f
Chyanne Haugen <42098386+crhaugen@users.noreply.github.com>
2019-03-25 12:58:14 -0700

    Update binaryConverter.ino

--- binaryConverter.ino
+++ binaryConverter.ino
@@ -19,6 +19,7 @@ int leds[LED_NUM] = {28, 29, 30, 31, 32, 33, 34, 35};
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
+//init all the leds to output
 void setup()
 {
   Serial.begin(9600);
